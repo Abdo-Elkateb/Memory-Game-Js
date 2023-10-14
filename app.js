73b6b41ce8844316edacc0ace022508584671fa6
Abdo Elkateb <93152380+Abdo-Elkateb@users.noreply.github.com>
2023-10-14 12:57:23 +0200

    done

--- app.js
+++ app.js
@@ -1,76 +1,112 @@
-const grid = document.querySelector('#grid');
-
+let grid = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenId = []
+let cardId = []
+let cardWon = []
 
 let gridArry = [
     {
-        name: 'blank',
-        img: '/image/blank.png'
-    },
-    {
-        name: 'cheesburger',
-        img: '/image/cheeseburger.png'
-    },
-    {
         name: 'fries',
-        img: '/image/fries.png'
+        img: 'images/fries.png'
     },
     {
-        name: 'hotdog',
-        img: '/image/hotdog.png'
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
     },
     {
-        name: 'iceCream',
-        img: '/image/ice-cream.png'
-    },
-    {
-        name: 'milkShake',
-        img: '/image/milkshake.png'
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
     },
     {
         name: 'pizza',
-        img: '/image/pizza.png'
+        img: 'images/pizza.png'
     },
     {
-        name: 'white',
-        img: '/image/white.png'
+        name: 'milkshake',
+        img: 'images/milkshake.png'
     },
     {
         name: 'hotdog',
-        img: '/image/hotdog.png'
+        img: 'images/hotdog.png'
     },
     {
-        name: 'iceCream',
-        img: '/image/ice-cream.png'
+        name: 'fries',
+        img: 'images/fries.png'
     },
     {
-        name: 'milkShake',
-        img: '/image/milkshake.png'
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
     },
-
     {
-        name: 'white',
-        img: '/image/white.png'
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
     }
 ]
 console.log(gridArry)
-gridArry.sort(() => 0.5 - Math.random())
 console.log(gridArry)
+gridArry.sort(() => 0.5 - Math.random())
 
 function createBorad() {
     for (i = 0; i < gridArry.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'image/blank.png')
+        card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        card.addEventListener('click', flipCard)
         grid.appendChild(card)
+        card.addEventListener('click', flipCard)
+    
     }
 }
 createBorad()
-const cardsChon = []
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTowId = cardsChosenId[1]
+    console.log(cards)
+    if (optionOneId == optionTowId) {
+        alert("you have cliked")
+    }
+    // alert('done');
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert("you found a match")
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTowId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTowId].removeEventListener('click', flipCard)
+        cardWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', '/images/blank.png')
+        cards[optionTowId].setAttribute('src', '/images/blank.png')
+        alert("sorry try again")
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardWon.length
+    if (cardWon.length == gridArry.length/ 2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+}
+
 function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChon.push(gridArry[cardId].name)
-    console.log(cardsChon)
-    this.setAttribute("src", gridArry[cardId].img)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(gridArry[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', gridArry[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
 
 }
